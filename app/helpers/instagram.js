c10cdf5a871bcc0d838a3b632d6eba81996288b6
Adam Burvill <runtothehills@gmail.com>
2015-07-28 01:25:48 -0700

    Successfully getting a return from the instagram api

--- app/helpers/instagram.js
+++ app/helpers/instagram.js
@@ -8,7 +8,7 @@ api.use({
 var redirect_uri = 'http://192.168.59.103/handleauth';
 
 exports.authorize_user = function(req, res) {
-    res.redirect(api.get_authorization_url(redirect_uri, { scope: ['likes'], state: 'a state' }));
+    res.redirect(api.get_authorization_url(redirect_uri, { scope: ['relationships'], state: 'a state' }));
 };
 
 exports.handleauth = function(req, res) {
@@ -17,8 +17,13 @@ exports.handleauth = function(req, res) {
             console.log(err.body);
             res.send("Didn't work");
         } else {
-            console.log('Yay! Access token is ' + result.access_token);
-            res.send('You made it!!');
+            console.log(result.user.id);
+            api.use({ access_token: result.access_token });
+            api.user_follows(result.user.id, function(err, users, pagination, remaining, limit) {
+                console.log('err:', err);
+                console.log('users:', users);
+                res.send(JSON.stringify(users));
+            });
         }
     });
 };
