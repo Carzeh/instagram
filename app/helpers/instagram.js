0657f456671152886c540d01e31957b4019e73aa
Adam Burvill <runtothehills@gmail.com>
2015-07-29 00:58:07 -0700

    Retrieving an image from the first user in the accessed persons user list

--- app/helpers/instagram.js
+++ app/helpers/instagram.js
@@ -5,7 +5,7 @@ api.use({
     client_secret: "793f8682c4f845a1a668b9fbc5ac20f6"
 });
 
-var redirect_uri = 'http://192.168.59.103/handleauth';
+var redirect_uri = 'http://192.168.59.104/handleauth';
 
 exports.authorize_user = function(req, res) {
     res.redirect(api.get_authorization_url(redirect_uri, { scope: ['relationships'], state: 'a state' }));
@@ -20,9 +20,11 @@ exports.handleauth = function(req, res) {
             console.log(result.user.id);
             api.use({ access_token: result.access_token });
             api.user_follows(result.user.id, function(err, users, pagination, remaining, limit) {
-                console.log('err:', err);
-                console.log('users:', users);
-                res.send(JSON.stringify(users));
+                var firstUser = users[0];
+
+                api.user_media_recent(firstUser.id, function(err, medias, pagination, remaining, limit) {
+                    res.send(JSON.stringify(medias[0].likes.count));
+                });
             });
         }
     });
