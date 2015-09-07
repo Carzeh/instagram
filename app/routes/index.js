1662f777795397f43206cc088409f849fe0114b4
Adam Burvill <runtothehills@gmail.com>
2015-09-07 09:12:21 -0700

    Stripped out all the instagram code and starting to get setup with the Google Maps API and translink code

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,9 @@
 var express = require('express');
-var instagram = require('../helpers/instagram');
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
-
-router.get('/handleauth', instagram.handleauth);
-router.get('/authorize_user', instagram.authorize_user);
 
 module.exports = router;
